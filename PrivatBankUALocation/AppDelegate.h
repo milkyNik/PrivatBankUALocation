1bcc6ecc3b66ee807e4b44824ab331a93c0f7b11
milkyNik <milniklaus@gmail.com>
2016-11-17 17:13:43 +0200

    Initial Commit

---
+++ PrivatBankUALocation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PrivatBankUALocation
+//
+//  Created by iMac on 17.11.16.
+//  Copyright © 2016 hata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
