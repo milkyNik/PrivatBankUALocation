bdd371c82910349a549db62dc3eae11d46c4c108
milkyNik <milniklaus@gmail.com>
2016-11-28 21:21:49 +0200

    Добавлен ключ

--- PrivatBankUALocation/AppDelegate.m
+++ PrivatBankUALocation/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <GoogleMaps.h>
 
 @interface AppDelegate ()
 
@@ -16,6 +17,9 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+    [GMSServices provideAPIKey:@"AIzaSyAjcSOFSDrJsK3gG1Yp1VJpRO4_AbKeK2o"];
+    
     // Override point for customization after application launch.
     return YES;
 }
